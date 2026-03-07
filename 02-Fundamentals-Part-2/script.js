34277700b1634f0768288d94df0407c4d66f9e24
Icefowz <maimoy16@gmail.com>
2026-03-07 17:02:54 +0200

    Using Strict Mode/Function

---
+++ 02-Fundamentals-Part-2/script.js
@@ -0,0 +1,39 @@
+"use strict";
+/*
+///////////////////////////////////////
+// Activating Strict Mode
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive :D');
+
+// const interface = 'Audio';
+// const private = 534;*/
+
+// 1
+
+// function logger() {
+//   console.log("myname is Manos");
+// }
+// // calling / running / invoking function
+// logger();
+// logger();
+// logger();
+
+// function fruitProcessor(pineapples, banana) {
+//   const juice = `juice is made with ${pineapples} pineapples and ${banana} banana.`;
+//   return juice;
+// }
+
+// const tropicalJuice = fruitProcessor(5, 7);
+// console.log(tropicalJuice);
+
+// const num = Number('23');
+// function describeCountry(country, population, capitalCity) {
+//   const description = `${country} has  ${population} million people and its capital is ${capitalCity}`;
+//   return description;
+// }
+
+// const result = describeCountry("Greece", 6, "Athens");
+// console.log(result);
